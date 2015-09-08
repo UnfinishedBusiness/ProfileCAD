eb47735edfb00e52cd4529bcdc97937d60822512
UnfinishedBusiness <travis.gillin@gmail.com>
2015-09-08 06:36:44 -0400

    Added osx build directory

---
+++ build_osx/CMakeFiles/ProfileCAD.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/admin/Documents/Projects/ProfileCAD/src/CAD.cpp" "/Users/admin/Documents/Projects/ProfileCAD/build_osx/CMakeFiles/ProfileCAD.dir/CAD.cpp.o"
+  "/Users/admin/Documents/Projects/ProfileCAD/src/Geometry.cpp" "/Users/admin/Documents/Projects/ProfileCAD/build_osx/CMakeFiles/ProfileCAD.dir/Geometry.cpp.o"
+  "/Users/admin/Documents/Projects/ProfileCAD/src/Keyboard.cpp" "/Users/admin/Documents/Projects/ProfileCAD/build_osx/CMakeFiles/ProfileCAD.dir/Keyboard.cpp.o"
+  "/Users/admin/Documents/Projects/ProfileCAD/src/Menu.cpp" "/Users/admin/Documents/Projects/ProfileCAD/build_osx/CMakeFiles/ProfileCAD.dir/Menu.cpp.o"
+  "/Users/admin/Documents/Projects/ProfileCAD/src/Mouse.cpp" "/Users/admin/Documents/Projects/ProfileCAD/build_osx/CMakeFiles/ProfileCAD.dir/Mouse.cpp.o"
+  "/Users/admin/Documents/Projects/ProfileCAD/src/Scene.cpp" "/Users/admin/Documents/Projects/ProfileCAD/build_osx/CMakeFiles/ProfileCAD.dir/Scene.cpp.o"
+  "/Users/admin/Documents/Projects/ProfileCAD/src/UI.cpp" "/Users/admin/Documents/Projects/ProfileCAD/build_osx/CMakeFiles/ProfileCAD.dir/UI.cpp.o"
+  "/Users/admin/Documents/Projects/ProfileCAD/src/Window.cpp" "/Users/admin/Documents/Projects/ProfileCAD/build_osx/CMakeFiles/ProfileCAD.dir/Window.cpp.o"
+  "/Users/admin/Documents/Projects/ProfileCAD/src/main.cpp" "/Users/admin/Documents/Projects/ProfileCAD/build_osx/CMakeFiles/ProfileCAD.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/admin/Documents/Projects/ProfileCAD/src/../inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ build_osx/CMakeFiles/ProfileCAD.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProfileCAD.dir/main.cpp.o"
+  "CMakeFiles/ProfileCAD.dir/Scene.cpp.o"
+  "CMakeFiles/ProfileCAD.dir/Keyboard.cpp.o"
+  "CMakeFiles/ProfileCAD.dir/Menu.cpp.o"
+  "CMakeFiles/ProfileCAD.dir/Window.cpp.o"
+  "CMakeFiles/ProfileCAD.dir/Mouse.cpp.o"
+  "CMakeFiles/ProfileCAD.dir/CAD.cpp.o"
+  "CMakeFiles/ProfileCAD.dir/UI.cpp.o"
+  "CMakeFiles/ProfileCAD.dir/Geometry.cpp.o"
+  "ProfileCAD.pdb"
+  "ProfileCAD.app/Contents/MacOS/ProfileCAD"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProfileCAD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
