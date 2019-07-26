0b6d03d4fbc94fb9d5d95d5b95756ff92cc54f45
UnfinishedBusiness <travis.gillin@gmail.com>
2019-07-26 07:43:05 -0400

    Line tool works but no manual input yet

--- src/CAD/drawing_tools.js
+++ src/CAD/drawing_tools.js
@@ -1,3 +1,5 @@
+var snap_pointer;
+var active_part = "test"
 function UnSelectAll()
 {
 	for (var x = 0; x < render.Stack.length; x++)
@@ -30,7 +32,7 @@ function SelectAll()
 }
 function showSnapPointer()
 {
-  var part = render.newLivePart("snap_pointer");
+  snap_pointer = render.newLivePart("snap_pointer");
   var e = {
     type: "circle",
     origin: [0, 0],
@@ -42,15 +44,8 @@ function showSnapPointer()
     var z_factor = render.camera.position.z * 0.1;
     e.radius = (z_factor * render._mouseOverPadding) / 2;
   };
-  e.meta.on_mouse_click = function(e){
-    e.meta.on_mouse_motion = null;
-    var new_entity = render.copy_obj(e);
-    //console.log("Creating line: " + JSON.stringify(new_entity));
-    render.removePartFromScene("snap_pointer");
-    return true; //true - Delete the callback after it returns
-  };
-  part.entities.push(e);
-  render.Stack.push(part);
+  snap_pointer.entities.push(e);
+  render.Stack.push(snap_pointer);
 }
 function hideSnapPointer()
 {
@@ -66,16 +61,24 @@ function Line()
     end: [0, 0],
     meta: render.copy_obj(render._liveMeta)
   }
-  e.meta.on_mouse_motion = function(e){
-    e.end = [render.snapPosition.x, render.snapPosition.y];
-  };
-  e.meta.on_mouse_click = function(e){
-    e.meta.on_mouse_motion = null;
-    var new_entity = render.copy_obj(e);
-    //console.log("Creating line: " + JSON.stringify(new_entity));
-    render.removePartFromScene("line_tool");
-		hideSnapPointer();
-    return true; //true - Delete the callback after it returns
+	showSnapPointer();
+	e.meta.on_mouse_click = function(e){
+		e.origin = [render.snapPosition.x, render.snapPosition.y];
+		e.end = [render.snapPosition.x, render.snapPosition.y];
+		e.meta.on_mouse_motion = function(e){
+	    e.end = [render.snapPosition.x, render.snapPosition.y];
+	  };
+	  e.meta.on_mouse_click = function(e){
+	    e.meta.on_mouse_motion = null;
+	    var new_entity = render.copy_obj(e);
+			new_entity.meta.color = "white";
+	    //console.log("Creating line: " + JSON.stringify(new_entity));
+	    render.removePartFromStack("line_tool");
+			hideSnapPointer();
+			render.addEntityToPart(active_part, new_entity);
+	    return true; //true - Delete the callback after it returns
+	  };
+    return false; //true - Delete the callback after it returns
   };
   part.entities.push(e);
   render.Stack.push(part);
